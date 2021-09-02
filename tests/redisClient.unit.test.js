6a63db0558a9e371ec9023bcdab3c6001140d5d8
Satyam Rathod <rathodsatyam33@gmail.com>
2021-09-02 15:48:40 +0530

    198555766 Add unit test cases for redis client
      Added new test cases for redis client feature

---
+++ tests/redisClient.unit.test.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const assert = require('assert');
+const RedisClientLib = require('../lib/redisClient');
+
+
+describe('redis client', () => {
+
+    it('return undefined if key is not present', done => {
+        
+        const redisClient = new RedisClientLib({ redisHost: '127.0.0.1', redisPort: 6379 }, (error) => {
+            if (error) {
+                console.log(error)
+            }
+            
+            redisClient.read('testKey', (err, val) => {
+                assert.equal(val,undefined)
+            })
+        });
+        done();
+    });
+
+    it('returns value for valid key', done => {
+        
+        const redisClient = new RedisClientLib({ redisHost: '127.0.0.1', redisPort: 6379 }, (error) => {
+            if (error) {
+                console.log(error)
+            }
+
+            redisClient.write('testKey','testValue', (err, val) => {
+                if (err) {
+                    console.log(err)
+                }
+            });
+            
+            redisClient.read('testKey', (err, val) => {
+                assert.equal(val, 'testValue')
+            });
+        });
+        done();
+    });
+
+
+})
